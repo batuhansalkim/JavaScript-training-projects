65f9dfe14ce3b3afc442b723fa73e2bafe805371
Baturpcu <batusalkim11@gmail.com>
2022-12-13 23:44:46 +0300

    update
    update

--- JavaScript/4/hex.js
+++ JavaScript/4/hex.js
@@ -7,10 +7,13 @@ const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function(btn){
     btn.addEventListener("click",function(e){
+
         const styles =  e.currentTarget.classList;
-        //console.log(styles);
+       // console.log(styles);
+
         if(styles.contains("decrease")){
             count--;
+           
         }
         else if(styles.contains("increase")){
             count++
@@ -21,12 +24,17 @@ btns.forEach(function(btn){
         
         if(count > 0){
             value.style.color="green";
+            btn.style.background="green";
+            btn.style.color="white";
         }
         if(count <0){
             value.style.color = "red";
+            btn.style.background="red";
         }
         if(count=== 0){
             value.style.color = "#222";
+            btn.style.background="black";
+            btn.style.color="white";
         }
         value.textContent = count;
 
