769a16e390eac8bbb3a0c3a1b0a34b98270c4e4e
Baturpcu <batusalkim11@gmail.com>
2022-12-17 20:36:19 +0300

    update

--- JavaScript/8sinema/hex.js
+++ JavaScript/8sinema/hex.js
@@ -2,48 +2,48 @@ const container = document.querySelector(".container");
 const count = document.getElementById("count");
 const amount = document.getElementById("amount");
 const select = document.getElementById("movie");
-
-
+const seats = document.querySelectorAll(".seat:not(.reserved)");
 
 container.addEventListener("click",function(e){
     if(e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){
         e.target.classList.toggle("selected");
-
-        let seciliKoltukSayisi = container.querySelectorAll(".seat.selected").length;
-        let price = select.value;
-
-        count.innerText=seciliKoltukSayisi;
-        amount.innerText =seciliKoltukSayisi*  price;
+        calculateTotal(); 
     }
-})
+});
 
 
 select.addEventListener("change",function(e){
-
+    calculateTotal();
 });
 
 
 
+function calculateTotal(){
+    const selectedSeats = container.querySelectorAll(".seat.selected");
+
+    const selectedSeatsArr = [];
+    const seatArr = [];
+
+    selectedSeats.forEach(function(seat){
+        selectedSeatsArr.push(seat);
+    });
 
 
+    seats.forEach(function(seat){
+        seatArr.push(seat);
+    });
 
+    //[1,3,5]
+    let selectedSeatIndex = selectedSeatsArr.map(function(seat){
+        return seatArr.indexOf(seat);
+    });
 
+    console.log(selectedSeatIndex);
 
+    //spread
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let SelectedSeatCount = selectedSeats.length;
+    count.innerText = SelectedSeatCount;
+    amount.innerText = SelectedSeatCount * select.value;
+}
 
